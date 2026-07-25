59f834737db3c176975fab2102980cd0fb1bce76
ZelsL <emersonlaureanonas@gmail.com>
2026-07-25 12:44:18 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,23 @@
-import { Client } from "pg";
+import pg from "pg";
+
+const { Pool } = pg;
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const res = await pool.query(queryObject);
+    return res;
+  } catch (err) {
+    console.error("Error to consult database: ", err);
+  }
 }
 
 export default {
   query: query,
 };
-
-//docker compose -f infra/compose.yaml up -d
