55309049ec15bf98870db804ea7d9f84faea19e7
ZelsL <emersonlaureanonas@gmail.com>
2026-07-25 13:30:58 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,18 @@
 import pg from "pg";
 
+function getSSLConfig() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      rejectUnauthorized: true,
+      ca: process.env.POSTGRES_CA,
+    };
+  } else if (!process.env.POSTGRES_CA) {
+    return false;
+  }
+
+  return { rejectUnauthorized: false };
+}
+
 const { Pool } = pg;
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
@@ -7,6 +20,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
+  ssl: getSSLConfig(),
 });
 
 async function query(queryObject) {
@@ -15,6 +29,7 @@ async function query(queryObject) {
     return res;
   } catch (err) {
     console.error("Error to consult database: ", err);
+    throw err;
   }
 }
 
