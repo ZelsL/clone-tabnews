35fb0cc56dfc2622336e255a370cf1970e97ee7b
ZelsL <emersonlaureanonas@gmail.com>
2026-08-14 12:55:26 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/unsupported-methods.test.js
+++ tests/integration/api/v1/migrations/unsupported-methods.test.js
@@ -5,21 +5,27 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
-
-test("DELETE, PUT, PATCH to /api/v1/migrations should return 405 and end database connections", async () => {
-  const methods = ["DELETE", "PUT", "PATCH"];
-  for (const method of methods) {
-    const response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: method,
+describe("DELETE, PUT, PATCH /api/v1/migrations", () => {
+  describe("anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const methods = ["DELETE", "PUT", "PATCH"];
+      for (const method of methods) {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: method,
+          },
+        );
+        expect(response.status).toBe(405);
+      }
+      const databaseName = process.env.POSTGRES_DB;
+      const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+      });
+      const databaseOpenedConnectionsValue =
+        databaseOpenedConnectionsResult.rows[0].count;
+      expect(databaseOpenedConnectionsValue).toBe(1);
     });
-    expect(response.status).toBe(405);
-  }
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
   });
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
-  expect(databaseOpenedConnectionsValue).toBe(1);
 });
